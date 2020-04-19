6c20ac8b6be016595ca0f40533e657e9a9fdba79
jayrubis <s3546658@student.rmit.edu.au>
2020-04-19 17:41:24 +1000

    Update Tile.java

--- src/View/board/Tile.java
+++ src/View/board/Tile.java
@@ -6,16 +6,6 @@ import javax.swing.*;
 public class Tile extends JButton
 {
 	private String attribute;
-	public Tile(boolean isIsland)
-	{	
-	package View.board;
-
-import java.awt.*;
-import javax.swing.*;
-
-public class Tile extends JButton
-{
-	private String attribute;
 	
 	///////////////////////////////////////////////////
 	public Tile(String type)
@@ -77,7 +67,6 @@ public class Tile extends JButton
 		{
 			attribute = "island";
 			setBackground(Color.GRAY);
-			setText("HELLO");
 		}
 		else
 		{
@@ -102,33 +91,5 @@ public class Tile extends JButton
 	{
 		return this.attribute;
 	}
-}
-	if(isIsland)
-		{
-			attribute = "island";
-			setBackground(Color.GRAY);
-		}
-		else
-		{
-			attribute = "ocean";
-			setBackground(Color.BLUE);
-		}
-	}
 	
-	public void setIsland()
-	{
-		attribute = "island";
-		setBackground(Color.GRAY);
-	}
-	
-	public void setOcean()
-	{
-		attribute = "ocean";
-		setBackground(Color.BLUE);
-	}
-	
-	public String getAttribute()
-	{
-		return this.attribute;
-	}
 }
