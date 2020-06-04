de4f4bec804b3c11420cfd2563b3cade2ead15b0
Mohammad.Farshidmehr <Mohammad.f@timetabling.com.au>
2020-06-04 14:35:57 +1000

    fix movement error

--- src/Controller/EgaleMouseActionListener.java
+++ src/Controller/EgaleMouseActionListener.java
@@ -9,7 +9,6 @@ import javax.swing.JOptionPane;
 import Entity.*;
 import Model.Model;
 import Patterns.Chain.AbstractLogger;
-import Patterns.State.Context.GameStatus;
 import View.Tile;
 import View.View;
 
@@ -148,101 +147,6 @@ public class EgaleMouseActionListener implements MouseListener
 			break;
 		default:
 			break;
-		}
-
-		if(model.getContext().getGameState() != GameStatus.EGALE)
-		{
-			//JOptionPane.showMessageDialog(null,"It is shark turn");
-			//			Tile tileItem = (Tile) e.getSource();
-			//			Shark sharkChoose = new Shark("");
-			//			for(Shark shark : model.getSharks())
-			//			{
-			//				if(shark.getName().equalsIgnoreCase(tileItem.getAttribute()))
-			//				{
-			//					sharkChoose = shark;
-			//				}
-			//			}
-			//			Eagle eagleChoose = new Eagle("");
-			//			if(tileItem != null && controller.checkMovement(tileItem.getRow()-view.getBoard().getSelectedRow(),
-			//					tileItem.getColumn()-view.getBoard().getSelectedColumn()))
-			//			{
-			//				for(Eagle eagle : model.getEagles())
-			//				{
-			//					if(eagle.getName().contains(tileItem.getAttribute()))
-			//					{
-			//						eagleChoose = eagle;
-			//					}
-			//				}
-			//				if(eagleChoose.getLife() > 0)
-			//				{
-			//					eagleChoose.reduceLife(sharkChoose.getLifeAbility());
-			//					if(eagleChoose.getLife() <= 0)
-			//					{
-			//						controller.doMovement(tileItem);
-			//					}
-			//				}
-			//			}
-			//			else
-			//			{
-			//				model.getLoggerChain().setwMessage(AbstractLogger.EAGLE, "Wrong selection");
-			//				view.updateEagleLog(model.getLoggerChain().message);
-			//			}
-		}
-		else 
-		{
-			//			Tile tile = (Tile) e.getSource();
-			//			Eagle eagleChoose = new Eagle("");
-			//			if(tile != null) 
-			//			{
-			//				if(Arrays.asList(model.eagles()).contains(tile.getAttribute()))
-			//				{
-			//					if(view.getBoard().getSelectedRow() == -1 && view.getBoard().getSelectedColumn() == -1) 
-			//					{
-			//						view.getBoard().setSelectedRow(tile.getRow());
-			//						view.getBoard().setSelectedColumn(tile.getColumn());
-			//						view.getBoard().setSelectedname(tile.getName());
-			//						model.getLoggerChain().setwMessage(AbstractLogger.EAGLE, tile.getAttribute() + " ( " +tile.getRow() + "," +tile.getColumn() + " )");
-			//						view.updateEagleLog(model.getLoggerChain().message);
-			//					}	
-			//				}
-			//				
-			//				for(Eagle eagle : model.getEagles())
-			//				{
-			//					if(eagle.getName().contains(tile.getAttribute()))
-			//					{
-			//						eagleChoose = eagle;
-			//					}
-			//				}
-			//				((Container) view.getCurrentAnimalPanel().getComponent(0)).removeAll();
-			//				JLabel currentLabel = new JLabel("This is the animal that you choose");
-			//				JLabel eagleName = new JLabel(eagleChoose.getName());
-			//				JLabel eagleLife = new JLabel("Life: " + String.valueOf(eagleChoose.getLife()));
-			//				JLabel movementType = new JLabel("Movement: in 3 tiles");
-			//				
-			//				JButton changeBehaviour = new JButton("Use Ability");
-			//				if(eagleChoose.getName().equalsIgnoreCase("Bald"))
-			//				{
-			//					MouseListener bald = new BaldMouseListener(eagleChoose,view);
-			//					changeBehaviour.addMouseListener(bald);	
-			//				}
-			//				else if(eagleChoose.getName().equalsIgnoreCase("Black"))
-			//				{
-			//					MouseListener black = new BlackMouseListener(eagleChoose,view);
-			//					changeBehaviour.addMouseListener(black);	
-			//				}
-			//				else if(eagleChoose.getName().equalsIgnoreCase("Bateleur"))
-			//				{
-			//					MouseListener bateleur = new BateleurMouseListener(eagleChoose,view);
-			//					changeBehaviour.addMouseListener(bateleur);	
-			//				}
-			//				
-			//				((Container) view.getCurrentAnimalPanel().getComponent(0)).add(currentLabel);
-			//				((Container) view.getCurrentAnimalPanel().getComponent(0)).add(eagleName);
-			//				((Container) view.getCurrentAnimalPanel().getComponent(0)).add(eagleLife);
-			//				((Container) view.getCurrentAnimalPanel().getComponent(0)).add(movementType);
-			//				((Container) view.getCurrentAnimalPanel().getComponent(0)).add(changeBehaviour);
-			//				((Container) view.getCurrentAnimalPanel().getComponent(0)).validate();
-			//			}
 		}
 	}
 
